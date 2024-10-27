037ea000d1381ca035bff5f27dd9e9c74320fbfd
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-10-27 22:13:35 +0545

    first Commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const { TodoApi, Sequelize, sequelize } = require("./models/todoApi");
+
+const app = express();
+const PORT = 8080;
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+  message: "To many request !!!",
+});
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// create a new Todo tasks
+app.post("/create", async (request, response) => {
+  try {
+    const todo = await TodoApi.create(request.body);
+    response.status(200).json({
+      message: "Task has been added",
+      task: todo,
+    });
+  } catch (error) {
+    console.log(error);
+    response.send("not updated");
+  }
+});
+
+// get all tasks from database
+app.get("/receive", async (request, response) => {
+  try {
+    const todo = await TodoApi.findAll();
+    if (todo) {
+      response.status(200).json({
+        message: "All Tasks are listed below:",
+        task: todo,
+      });
+    } else {
+      response.status(400).json({
+        message: "Todo task is not found!",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    response.send("not found any tasks");
+  }
+});
+
+// update tasks to database
+app.put("/update", async (request, response) => {
+  try {
+    const { id, ...updateData } = request.body;
+
+    if (!id) {
+      return response.status(400).json({
+        message: "Todo task id is required!!",
+      });
+    }
+
+    console.log(updateData);
+
+    const updated = await TodoApi.update(
+      { ...updateData },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    if (updated) {
+      return response.status(200).json({
+        message: "Todo tasks is updated!",
+        task: await TodoApi.findOne({ where: { id } }),
+      });
+    } else {
+      return response.status(400).json({
+        message: "Todo task is not found!",
+      });
+    }
+  } catch (error) {
+    response.send("no task to updated", error);
+  }
+});
+
+// delete tasks to database
+app.delete("/delete", async (request, response) => {
+  try {
+    const { id } = request.body;
+
+    if (!id) {
+      return response
+        .status(400)
+        .json({ message: "Todo tasks is required for deleted" });
+    }
+
+    const deleted = await TodoApi.destroy({
+      where: {
+        id: id,
+      },
+    });
+
+    if (deleted) {
+      return response.status(200).json({
+        message: "Successfully deleted",
+      });
+    } else {
+      response.status(404).json({
+        message: "Todo task is not found!",
+      });
+    }
+  } catch (error) {
+    response.status(400).json({ message: error });
+  }
+});
+
+//universal route
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database connect vako xa la sathi haruuuuuu.....");
+    })
+    .catch((error) => {
+      console.log("connect vako xaina ");
+    });
+});
---
+++ models/todoApi.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+const TodoApi = sequelize.define("Tasks", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  expiry_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  status: {
+    type: DataTypes.ENUM("pending", "expired", "completed"),
+    allowNull: false,
+    defaultValue: "pending",
+  },
+});
+
+module.exports = { TodoApi, sequelize };
